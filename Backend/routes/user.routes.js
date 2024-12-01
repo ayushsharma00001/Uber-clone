1f8fb5331077aeaea5be739f0ce3d04ba2f43cf4
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2024-12-01 12:10:57 +0530

    updating user authentication routes

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -9,14 +9,12 @@ router.post("/register",[
     body("email").isEmail().withMessage("Invalid Email..."),
     body("fullname.firstname").isLength({min:3}).withMessage("First name must be at least 3 characters long..."),
     body("password").isLength({min:6}).withMessage("password must be at least 6 characters long..."),
-    userController.registerUser
-]);
+],userController.registerUser);
 
 router.post("/login",[
     body("email").isEmail().withMessage("Invalid email..."),
     body("password").isLength({min:6}).withMessage("Password must be atleast 6 Character long..."),
-    userController.loginUser
-]);
+],userController.loginUser);
 
 
 router.get("/profile",authMiddleware.authUser,userController.getUserProfile);
